3dde29033e98775b1d4d20836686f9afe79d2be5
Shivansh Talwar <shivanshtalwar0@gmail.com>
2019-11-30 16:25:42 +0530

    quickinstall

---
+++ quickinstall.py
@@ -0,0 +1,3 @@
+import os
+print('initiated quick install')
+os.system('git clone https://github.com/shivanshtalwar0/squidproxyserver_debian_auto_installer_script.git && sudo python3 squidproxyserver_debian_auto_installer_script/install.py')
